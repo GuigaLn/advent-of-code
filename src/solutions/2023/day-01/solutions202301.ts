11285c48fc252c6eb44606571de10a1a0d1d27a4
GuilhermeNallon <116029163+GuilhermeNallon@users.noreply.github.com>
2023-12-05 07:58:05 -0300

    feat: 2023-Day-01 Part 02

--- src/solutions/2023/day-01/solutions202301.ts
+++ src/solutions/2023/day-01/solutions202301.ts
@@ -1,9 +1,34 @@
 export class Solutions202301 {
+  private DAY_MAP = {
+    ZERO: 0,
+    ONE: 1,
+    TWO: 2,
+    THREE: 3,
+    FOUR: 4,
+    FIVE: 5, 
+    SIX: 6,
+    SEVEN: 7,
+    EIGHT: 8,
+    NINE: 9,
+  } as { [key: string]: number };
+
   constructor(private readonly calibrationArray: string[]) {}
+
+  private replaceTextInNumber(text: string) {
+    let replaceText = text;
+
+    Object.keys(this.DAY_MAP).forEach(key => { 
+      const value = this.DAY_MAP[key];
+      replaceText = `${replaceText.replace(new RegExp(key, 'g'), `${key[0]}${value.toString()}${key[key.length - 1]}`)}`;
+    });
+
+    return replaceText;
+  }
 
   calibration() {
     return this.calibrationArray.reduce((prev, currentValue) => {
-      const currentValueFilter = currentValue.split('').filter((value) => Number(value));
+      const currentValueSubstituted = this.replaceTextInNumber(currentValue.toUpperCase());
+      const currentValueFilter = currentValueSubstituted.split('').filter((value) => Number(value));
 
       if(currentValueFilter.length === 0) return prev;
 
